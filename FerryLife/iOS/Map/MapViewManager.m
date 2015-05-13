9058bfcc15a416da4f88fc33a645c5548f73e374
Mars Hall <m@marsorange.com>
2015-05-12 17:42:27 -0700

    Revise Xcode project org name

--- FerryLife/iOS/Map/MapViewManager.m
+++ FerryLife/iOS/Map/MapViewManager.m
@@ -3,7 +3,7 @@
 //  FerryLife
 //
 //  Created by Mars Hall on 2015-05-12.
-//  Copyright (c) 2015 Facebook. All rights reserved.
+//  Copyright (c) 2015 Mars Hall. All rights reserved.
 //
 
 #import "MapViewManager.h"
