289363ffbf25331f2bac06bb0bc837d8e6549913
Mars Hall <m@marsorange.com>
2015-05-13 19:15:06 -0700

    Refine layout of the Ferry Map

--- FerryLife/src/ferry-map.jsx
+++ FerryLife/src/ferry-map.jsx
@@ -2,21 +2,25 @@ import React from 'react-native';
 
 let {
   requireNativeComponent,
-  StyleSheet } = React;
+  StyleSheet,
+  View } = React;
 
 let styles = StyleSheet.create({
   scene: {
+    flex: 1
+  },
+  map: {
     flex: 1,
-    padding: 10,
-    paddingTop: 74,
     width: 325,
-    height: 405
+    alignSelf: 'center'
   }
 });
 
 class FerryMapView extends React.Component {
   render() {
-    return <NativeFerryMapView style={styles.scene} {...this.props} />;
+    return <View style={styles.scene}>
+      <NativeFerryMapView style={styles.map} {...this.props} />
+    </View>;
   }
 }
 
